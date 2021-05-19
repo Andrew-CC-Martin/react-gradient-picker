4eb3d128dfb3a626bea30aa7ffe33319ba86748e
Andrew-CC-Martin <amartin262@gmail.com>
2021-05-19 15:42:35 +1000

    add degrees rotation

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ class App extends React.Component {
   state = {
     colors: [
       "#000000", "#000000"
-    ]
+    ],
+    degreesRotation: "0"
   }
 
   componentDidMount() {
     const colors = localStorage.getItem("colors")
     colors && this.setState({ colors: JSON.parse(colors) })
+
+    const degreesRotation = localStorage.getItem("degreesRotation")
+    degreesRotation && this.setState({ degreesRotation })
   }
 
   updateColors = (event, i) => {
@@ -22,25 +26,51 @@ class App extends React.Component {
     localStorage.setItem("colors", JSON.stringify(newColors))
   }
 
+  updateRotation = (event) => {
+    const { value } = event.target
+    if (value) {
+      this.setState({ degreesRotation: value })
+
+      localStorage.setItem("degreesRotation", value)
+    }
+  }
+
   render() {
-    const { colors } = this.state
-    const css = `background-image: linear-gradient(${colors[0]}, ${colors[1]});`
+    const { colors, degreesRotation } = this.state
+    const css = `background-image: linear-gradient(${degreesRotation}deg, ${colors[0]}, ${colors[1]});`
 
     return (
-      <MainDiv colors={colors}>
+      <MainDiv colors={colors} degreesRotation={degreesRotation}>
         <h1>Background gradient picker</h1>
+
         <div>
+          <label htmlFor="degrees">
+            Degrees Rotation
+          </label>
+          <input
+            name="degrees"
+            type="number"
+            onChange={this.updateRotation}
+            value={degreesRotation}
+            max={359}
+            min={0}
+          />
           {colors.map((color, i) => {
             return (
-              <input
-                type="color"
-                value={color}
-                onChange={(event) => this.updateColors(event, i)}
-                key={i}
-              />
+              <React.Fragment key={i}>
+                <label htmlFor="degrees">
+                  Color {i + 1}
+                </label>
+                <input
+                  type="color"
+                  value={color}
+                  onChange={(event) => this.updateColors(event, i)}
+                />
+              </React.Fragment>
             )
           })}
         </div>
+
         <h4>
           Your CSS is:
         </h4>
--- src/styled-components/index.jsx
+++ src/styled-components/index.jsx
@@ -1,10 +1,12 @@
 import styled from "styled-components"
 
+const backgroundCallback = ({ colors, degreesRotation }) => `${degreesRotation}deg, ${colors[0]},${colors[1]}`
+
 export const MainDiv = styled.div`
 display: flex;
 flex-direction: column;
 align-items: center;
-background-image: linear-gradient(${({ colors }) => `${colors[0]},${colors[1]}`});
+background-image: linear-gradient(${backgroundCallback});
 height: 100%;
 width: 100%;
 `
